9896beb562872f67a9f198672445d67e364d7df0
ayoub-ettabetCEI <92469479+ayoub-ettabetCEI@users.noreply.github.com>
2021-10-13 22:00:39 +0100

    ejer 1

---
+++ practica/js/main.js
@@ -0,0 +1,8 @@
+
+// tomar informacion de los formularios
+const inputName = document.querySelector("#nombre");
+inputName.classList.add("title");
+console.log(inputName.value);
+
+const inputApellido = document.querySelector("#apellido");
+console.log(inputApellido.value)
