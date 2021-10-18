14d27b3dbba5e132c05fbb0f6642a274fa31337a
ayoub-ettabetCEI <92469479+ayoub-ettabetCEI@users.noreply.github.com>
2021-10-18 18:25:16 +0100

    ejer

---
+++ ejer 2- operadores/js/main.js
@@ -0,0 +1,27 @@
+console.log(`readyyyy :)))`);
+
+//tipos
+const precioProducto = 150;
+const precio = 100.90
+
+// STRING
+const name = "Ayoub";
+
+// booleanos true-false
+const hijos = true;
+
+// undefined
+ const apellido = undefined;
+ const title = document.querySelector("title");
+ console.log(title.value);
+
+
+//  Operaciones aritmeticas +- / * %
+
+const numA = 50;
+const numB = 20;
+
+const resultado = numA + numB;
+console.log(resultado);
+
+console.log("el resultado es " + resultado);
